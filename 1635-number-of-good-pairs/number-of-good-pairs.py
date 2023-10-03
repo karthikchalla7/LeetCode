d152f79fd070b9e5d8e1d12a3bd8c96bcb110f77
karthik challa <76682351+karthikchalla7@users.noreply.github.com>
2023-10-03 09:40:31 +0530

    Time: 50 ms (5.92%) | Memory: 16.2 MB (53.10%) - LeetSync

--- 1635-number-of-good-pairs/number-of-good-pairs.py
+++ 1635-number-of-good-pairs/number-of-good-pairs.py
@@ -1,9 +1,20 @@
 class Solution:
     def numIdenticalPairs(self, nums: List[int]) -> int:
-        #brute force
-        pairs =0
-        for i in range(len(nums)):
-            for j in range(i+1,len(nums)):
-                if (i<j and nums[i]==nums[j]):
-                    pairs+=1
+        # #brute force
+        # pairs =0
+        # for i in range(len(nums)):
+        #     for j in range(i+1,len(nums)):
+        #         if (i<j and nums[i]==nums[j]):
+        #             pairs+=1
+        # return pairs
+
+        #one pass
+        store = {}
+        pairs = 0
+        for num in nums:
+            if num in store:
+                pairs+=store[num]
+                store[num]+=1
+            else:
+                store[num]=1
         return pairs
