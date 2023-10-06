78729d94dad9c5d3787b45bb0ef34a0b4e9de58e
karthik challa <76682351+karthikchalla7@users.noreply.github.com>
2023-10-06 11:34:42 +0530

    Time: 41 ms (75.45%) | Memory: 16.4 MB (40.77%) - LeetSync

--- 88-merge-sorted-array/merge-sorted-array.py
+++ 88-merge-sorted-array/merge-sorted-array.py
@@ -3,9 +3,16 @@ class Solution:
         """
         Do not return anything, modify nums1 in-place instead.
         """
+        # two pointer apporach
+        i = m-1
+        j = n-1
+        k = m+n-1
         
-        #brute froce approch
-        for i in range(n):
-            nums1[m+i]=nums2[i]
-        nums1.sort()
-        
+        while(j>=0):
+            if (i>=0 and nums1[i]>nums2[j]):
+                nums1[k]=nums1[i]
+                i-=1
+            else:
+                nums1[k]=nums2[j]
+                j-=1
+            k-=1
