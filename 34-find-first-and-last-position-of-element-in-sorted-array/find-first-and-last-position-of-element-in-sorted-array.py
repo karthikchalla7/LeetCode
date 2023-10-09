c57a8236253cb06975545c8dc7b8c24be17e1112
karthik challa <76682351+karthikchalla7@users.noreply.github.com>
2023-10-09 15:53:57 +0530

    Time: 73 ms (97.56%) | Memory: 17.5 MB (97.83%) - LeetSync

--- 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
+++ 34-find-first-and-last-position-of-element-in-sorted-array/find-first-and-last-position-of-element-in-sorted-array.py
@@ -1,16 +1,37 @@
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        first = -1
-        second =-1
-        for i in range(len(nums)):
-            if(nums[i]==target):
-                if first ==-1:
-                    first = i
-                second = i
-        return [first,second]
         
-        # for i in range(len(nums)-1,-1,-1):
-        #     if(nums[i]==target ):
-        #         second = i
-        #         break
-        # return [first,second]
+        def findfirst(nums,target):
+            first = -1
+            left = 0
+            right = len(nums)-1
+            while(left<=right):
+                mid = left+(right-left)//2
+                if(nums[mid]==target):
+                    first = mid
+                    right = mid-1
+                elif nums[mid]<target:
+                    left = mid+1
+                else:
+                    right=mid-1
+            return first
+
+        def findsecond(nums,target):
+            second = -1
+            left = 0
+            right = len(nums)-1
+            while(left<=right):
+                mid = (right-left)//2 + left
+                if(nums[mid]==target):
+                    second = mid
+                    left = mid+1
+                elif nums[mid]<target:
+                    left = mid+1
+                else:
+                    right=mid-1
+            return second
+
+        firstele = findfirst(nums,target)
+        secondele = findsecond(nums,target)
+        return [firstele,secondele]
+                
