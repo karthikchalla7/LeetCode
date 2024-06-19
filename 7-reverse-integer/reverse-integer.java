a227dce358293f36fce2712e969122eef4623c39
karthik challa <76682351+karthikchalla7@users.noreply.github.com>
2024-06-19 09:42:34 +0530

    Time: 1 ms (75.78%) | Memory: 40.8 MB (44.61%) - LeetSync

--- 7-reverse-integer/reverse-integer.java
+++ 7-reverse-integer/reverse-integer.java
@@ -1,12 +1,14 @@
 class Solution {
     public int reverse(int x) {
-        long res = 0;
+        
+        long rev = 0;
         while(x!=0){
-            int rem = x%10;
-            res=rem+res*10;
+            long rem = x%10;
+            rev = rem+rev*10;
             x/=10;
         }
-        if(res<Math.pow(-2,31)|| res>Math.pow(2,31)) return 0;
-        return (int)res;
+        //importatn part in this question are constraints
+        if(rev<=Math.pow(-2,31) || rev>=Math.pow(2,31) ) return 0;
+        return (int) rev;
     }
 }
