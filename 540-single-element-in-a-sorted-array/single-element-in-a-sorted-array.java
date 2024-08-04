4698ef2aea0b6965a07ef4c57997eb0101f0c813
karthik challa <76682351+karthikchalla7@users.noreply.github.com>
2024-08-04 08:18:50 +0530

    Time: 0 ms (100.00%) | Memory: 49.9 MB (84.52%) - LeetSync

--- 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
+++ 540-single-element-in-a-sorted-array/single-element-in-a-sorted-array.java
@@ -1,9 +1,25 @@
 class Solution {
     public int singleNonDuplicate(int[] nums) {
-        // we can use bit manipulation 
-        //naive approach but the problem demands o(logn)-binary search approach
-        int number = 0;
-        for(int num:nums) number^=num;
-        return number;   
+        //using binary search
+        //according to the pattern if the numbers appearing exaclty twice 
+        //so the pattern goes like this [evenindex,oddindex] but if a single is their then it disrupts the pattern
+
+        int left =0;
+        int right = nums.length-1;
+    
+        while(left<right){
+            int mid = left+(right-left)/2;
+            
+            //since paring starts with evenindex from observation.
+            if(mid%2==1) mid--;
+
+            //since the pair is correct we move ahead of the pair
+            if(nums[mid]==nums[mid+1]) left = mid+2;
+            else right = mid;
+        }
+
+        
+        return nums[left];
+
     }
 }
