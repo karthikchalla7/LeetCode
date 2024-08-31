2702f96b04e1a84208e8a36eda8548ffe79c9142
karthik challa <76682351+karthikchalla7@users.noreply.github.com>
2024-08-31 18:33:15 +0530

    Time: 10 ms (92.38%) | Memory: 55.7 MB (95.44%) - LeetSync

--- 1325-path-with-maximum-probability/path-with-maximum-probability.java
+++ 1325-path-with-maximum-probability/path-with-maximum-probability.java
@@ -1,17 +1,19 @@
 class Solution {
     public double maxProbability(int n, int[][] edges, double[] succProb, int start_node, int end_node) {
-        
+
         double prob[] = new double[n];
         prob[start_node]=1.0;
+
+        //bellman ford but for max prob
         for(int i=0;i<n-1;i++){
             boolean updated = false;
             for(int j=0;j<edges.length;j++){
-                int u = edges[j][0];
+                int u =edges[j][0];
                 int v = edges[j][1];
                 double sp = succProb[j];
 
                 if(prob[u]*sp>prob[v]){
-                    prob[v] = prob[u]*sp;
+                    prob[v]=prob[u]*sp;
                     updated = true;
                 }
                 if(prob[v]*sp>prob[u]){
@@ -22,5 +24,6 @@ class Solution {
             if(!updated) break;
         }
         return prob[end_node];
+        
     }
 }
